c321d6c8d513e58b7b0076b346ce4887cdb08f15
sarashibu-cypto <sshibu@terpamil.umd.edu>
2026-09-24 18:15:57 -0400

    Second version of HTML About Me page

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -7,6 +6,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Sara Shibu!'
 
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
